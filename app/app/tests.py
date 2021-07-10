5ac019365ac24530c7beedad51dc78f9314c92c2
Your Name <you@example.com>
2021-07-10 20:54:58 +0530

    Dev

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from app.calc import add,subtract
 
+#testcase 1S
 class CalcTests(TestCase):
     def test_add_nummbers(self):
          """ test that two number added together"""
